cb673a8824fb2e2fbff2399f27a9aa7ce9315bb4
Shahriar <shahriarh655@gmail.com>
2024-12-24 13:34:50 +0600

    Implemented ts node dev for development environment and fix some minor issues.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
-
+const PORT = config.port || 3000;
 async function main() {
   try {
     await mongoose.connect(config.db_url as string);
     console.log('🚀 Database Connected Successfully');
 
-    app.listen(config.port, () => {
-      console.log(`🚀 Gearly Server Is Listening On Port => ${config.port}`);
+    app.listen(PORT, () => {
+      console.log(
+        `🚀 Gearly Server Is Listening On Port => http://localhost:${PORT}`,
+      );
     });
   } catch (err) {
     console.error('❌ Failed To Connected With Database', err);
