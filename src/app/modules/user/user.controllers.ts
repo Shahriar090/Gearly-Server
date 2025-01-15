1383f04ac34e2ec8aa33e0ad0d7d5202c95509ed
Shahriar <shahriarh655@gmail.com>
2025-01-15 15:34:06 +0600

    Updated user controllers. Fixed a bug from create admin controller function.

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { userServices } from './user.services';
 
-const createAdmin = async (req: Request, res: Response) => {
+const createAdmin = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await userServices.createAdminIntoDb(req.body.admin);
     if (result) {
@@ -10,16 +10,12 @@ const createAdmin = async (req: Request, res: Response) => {
         data: result,
       });
     } else {
-      return res.status(400).json({
+      res.status(400).json({
         message: 'Failed To Create Admin',
       });
     }
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      message: 'Internal Server Error',
-      error: 'An unexpected error occurred. Please try again later.',
-    });
+    next(error);
   }
 };
 
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { userControllers } from './user.controllers';
+
 const router = express.Router();
 // create admin
 router.route('/users/create-admin').post(userControllers.createAdmin);
+
 export const userRoutes = router;
