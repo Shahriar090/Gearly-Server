ca197796fc463e14973f135740224e6e9a77b5d2
Shahriar <shahriarh655@gmail.com>
2025-01-18 19:02:44 +0600

    Updated app.ts and user.routes.ts files. Implemented a separate routes folder to organize the routing configuration for the entire application.

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { userRoutes } from './app/modules/user/user.routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 // parser
 app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1', userRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('👋 Hello From Gearly E-Commerce Server!');
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -3,14 +3,14 @@ import { userControllers } from './user.controllers';
 
 const router = express.Router();
 // create user
-router.route('/users/create-user').post(userControllers.createUser);
+router.route('/create-user').post(userControllers.createUser);
 // get all users
-router.route('/users').get(userControllers.getAllUsers);
+router.route('/').get(userControllers.getAllUsers);
 // get single user
-router.route('/users/:id').get(userControllers.getSingleUser);
+router.route('/:id').get(userControllers.getSingleUser);
 // update a user
-router.route('/users/:id').put(userControllers.updateUser);
+router.route('/:id').put(userControllers.updateUser);
 // delete a user
-router.route('/users/:id').delete(userControllers.deleteUser);
+router.route('/:id').delete(userControllers.deleteUser);
 
 export const userRoutes = router;
