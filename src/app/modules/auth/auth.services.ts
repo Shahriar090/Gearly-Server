91fc7bd95536fb17586a7b30282b93d55fef191f
Shahriar <shahriarh655@gmail.com>
2025-01-19 13:51:36 +0600

    Updated authentication services and implemented validations in the login service function.

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,7 +1,25 @@
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 
 const loginUser = async (payload: TLoginUser) => {
-  console.log(payload);
+  // check if the user is exists or not
+  const user = await User.isUserExists(payload.email);
+
+  if (!user) {
+    throw new Error('User Not Found');
+  }
+
+  // check if the user is blocked
+  const isDeleted = user?.isDeleted;
+  if (isDeleted) {
+    throw new Error('This User Is Deleted.!');
+  }
+
+  // check if the user status is blocked
+  const userStatus = user?.status;
+  if (userStatus === 'Blocked') {
+    throw new Error('This User Is Blocked.!');
+  }
 };
 
 export const authServices = {
