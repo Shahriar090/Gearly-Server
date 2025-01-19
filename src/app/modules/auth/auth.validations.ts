fbd4b26097b2c195b6e7fe823d90f9950189445b
Shahriar <shahriarh655@gmail.com>
2025-01-19 11:56:55 +0600

    Implemented interface and validation schema for auth module.

---
+++ src/app/modules/auth/auth.validations.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+const loginUserValidationSchema = z.object({
+  body: z.object({
+    email: z
+      .string({ required_error: 'Email Is Required' })
+      .email('Invalid Email Format')
+      .trim(),
+    password: z.string({ required_error: 'Password Is Required' }),
+  }),
+});
+
+export const authValidations = {
+  loginUserValidationSchema,
+};
