8a33ad529eecacdcda8837599f36a72bf851d05d
Shahriar <shahriarh655@gmail.com>
2025-02-01 14:33:08 +0600

    Updated product model module. Implemented interface and model for product model.

--- src/app/modules/productModel/productModel.interface.ts
+++ src/app/modules/productModel/productModel.interface.ts
@@ -1,10 +1,24 @@
 import { Types } from 'mongoose';
 
-// model (iphone 16)
+export type TReview = {
+  user: Types.ObjectId;
+  rating: number;
+  comment?: string;
+};
+
 export type TProductModel = {
   name: string;
   slug: string;
   description: string;
-  imageUrl: string;
-  subCategory: Types.ObjectId;
+  price: number;
+  discount?: number;
+  stock: number;
+  category: Types.ObjectId;
+  subCategory?: Types.ObjectId;
+  brand: string;
+  images: string[];
+  ratings?: number;
+  reviews?: TReview[];
+  isFeatured: boolean;
+  isDeleted: boolean;
 };
--- src/app/modules/productModel/productModel.model.ts
+++ src/app/modules/productModel/productModel.model.ts
@@ -1,52 +1,112 @@
 import { model, Schema } from 'mongoose';
-import slugify from 'slugify';
-import { TProductModel } from './productModel.interface';
+import { TProductModel, TReview } from './productModel.interface';
 
-// model schema (each brand's model. For example, Galaxy S23 Ultra From Samsung)
+const reviewSchema = new Schema<TReview>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
 
-const productModelSchema = new Schema<TProductModel>({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
+const productModelSchema = new Schema<TProductModel>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 100,
+    },
+    stock: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    subCategory: {
+      type: Schema.Types.ObjectId,
+      ref: 'SubCategory',
+    },
+    brand: {
+      type: String,
+      trim: true,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    ratings: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+    reviews: [reviewSchema],
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  subCategory: {
-    type: Schema.Types.ObjectId,
-    ref: 'SubCategory',
-    required: true,
-  },
-});
+  { timestamps: true },
+);
 
-productModelSchema.pre('find', function (next) {
-  this.populate('subCategory').where({
-    'subCategory.category.isDeleted': { $ne: true },
-  });
-  next();
-});
-
-productModelSchema.pre('validate', function (next) {
-  if (!this.slug) {
-    this.slug = slugify(this.name, { lower: true, strict: true });
+// This logic will automatically updates product ratings based on user reviews. That's why no need to manually calculate average rating in the frontend/backend.
+productModelSchema.pre('save', function (next) {
+  if (this.isModified('reviews')) {
+    if (!this.reviews || this.reviews.length === 0) {
+      this.ratings = 0;
+    } else {
+      const total = this.reviews.reduce(
+        (sum, review) => sum + review.rating,
+        0,
+      );
+      this.ratings = total / this.reviews.length;
+    }
   }
   next();
 });
-// brand specific model schema
-export const ProductModel = model<TProductModel>(
-  'ProductModel',
-  productModelSchema,
-);
+
+// model
+export const Product = model<TProductModel>('Product', productModelSchema);
