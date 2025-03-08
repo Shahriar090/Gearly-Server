d1c815e20f100a979bf962d7607bbdf2f9e2c86b
Shahriar <shahriarh655@gmail.com>
2025-03-08 16:27:30 +0600

    Final testing for cors issues

--- src/app.ts
+++ src/app.ts
@@ -18,12 +18,8 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
-
-app.options('*', cors());
 
 // application routes
 app.use('/api/v1', router);
