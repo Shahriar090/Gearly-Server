a720a621c6c00a5c9a872d83ca58e46674f4ba18
Shahriar <shahriarh655@gmail.com>
2025-03-08 16:12:29 +0600

    Fixing cors issues

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,8 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 
