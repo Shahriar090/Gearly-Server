60c1d2e2dbd14dedc6b4c9f7682a5248dd12637b
Shahriar <shahriarh655@gmail.com>
2025-03-10 20:45:25 +0600

    Fixing issues from user module

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -7,28 +7,25 @@ import { USER_ROLES } from './user.constant';
 
 const router = express.Router();
 // create user
-router.route('/create-user').post(
-  // upload.single('file'),
-  // (req: Request, res: Response, next: NextFunction) => {
-  //   req.body = JSON.parse(req.body.data);
-  //   next();
-  // },
-  validateRequest(userValidations.createUserValidationSchema),
-  userControllers.createUser,
-);
-// // get all users
-// router.route('/').get(auth(USER_ROLES.Admin), userControllers.getAllUsers);
-// // get single user
-// router.route('/get-single-user/:id').get(userControllers.getSingleUser);
-// // update a user
-// router
-//   .route('/update-user/:id')
-//   .put(
-//     validateRequest(userValidations.updateUserValidationSchema),
-//     userControllers.updateUser,
-//   );
-// // delete a user
-// router.route('/delete-user/:id').delete(userControllers.deleteUser);
+router
+  .route('/create-user')
+  .post(
+    validateRequest(userValidations.createUserValidationSchema),
+    userControllers.createUser,
+  );
+// get all users
+router.route('/').get(auth(USER_ROLES.Admin), userControllers.getAllUsers);
+// get single user
+router.route('/get-single-user/:id').get(userControllers.getSingleUser);
+// update a user
+router
+  .route('/update-user/:id')
+  .put(
+    validateRequest(userValidations.updateUserValidationSchema),
+    userControllers.updateUser,
+  );
+// delete a user
+router.route('/delete-user/:id').delete(userControllers.deleteUser);
 
 // user profile
 router
