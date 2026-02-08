18b34a71008defa1320d16231b3456f9f7efd01d
Shahriar Hossain <shahriarh655@gmail.com>
2026-02-08 18:14:38 +0600

    refactor(api): removed all specifications and filtering related fields from category model

--- src/app/modules/category/category.interface.ts
+++ src/app/modules/category/category.interface.ts
@@ -1,25 +1,14 @@
+import { ObjectId } from 'mongodb';
 import type { CATEGORY_STATUS } from './category.constants';
 
 export type TCategoryStatus = (typeof CATEGORY_STATUS)[keyof typeof CATEGORY_STATUS];
 
-export type TSpecifications = {
-	name: string;
-	type: 'string' | 'number' | 'boolean';
-	required: boolean;
-};
-
-export type TFilteringFields = {
-	groupName: string;
-	value: string[];
-};
-
 export type TCategory = {
+	parentId?: ObjectId;
 	name: string;
 	slug: string;
 	description: string;
 	imageUrl: string;
 	status: TCategoryStatus;
-	specifications?: TSpecifications[];
-	filteringFields: TFilteringFields[];
 	isDeleted: boolean;
 };
