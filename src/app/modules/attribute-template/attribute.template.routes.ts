63dc4d03f466219d82c89e62c98de43d827067bb
Shahriar Hossain <shahriarh655@gmail.com>
2026-04-27 20:44:09 +0600

    feat(api): implemented update route logic for attribute template

--- src/app/modules/attribute-template/attribute.template.routes.ts
+++ src/app/modules/attribute-template/attribute.template.routes.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AttributeTemplateControllers } from './attribute.template.controllers';
-import { attributeTemplateCreateSchema } from './attribute.template.validation';
+import { attributeTemplateCreateSchema, attributeTemplatePatchSchema } from './attribute.template.validation';
 const router = express.Router();
 
 // create
 router
 	.route('/create-template')
 	.post(validateRequest(attributeTemplateCreateSchema), AttributeTemplateControllers.createAttributeTemplate);
+
+// update
+router
+	.route('/update')
+	.patch(validateRequest(attributeTemplatePatchSchema), AttributeTemplateControllers.updateAttributeTemplate);
 
 export const templateRoutes = router;
